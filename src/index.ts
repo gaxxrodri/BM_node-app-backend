f837c4f1963af7420ae801dc5a1e30992c71ab89
GaxRodri <gaspar.unlp@gmail.com>
2024-04-05 20:09:36 +0200

    feat: initialize express server and mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+
+import orderRoutes from './routes/orderRoutes';
+
 const app = express();
+const PORT = process.env.PORT ?? 3000;
+const MONGO_URI =
+  process.env.MONGO_DB_URI ?? 'mongodb://mongodb:27017/recommendationsDB';
+const MONGO_URI_DEV = 'mongodb://localhost:27017/yourDatabase';
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!!!2');
-});
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+app.use('/orders', orderRoutes);
+
+mongoose
+  .connect(MONGO_URI_DEV)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
