1e5cd8da3f12a299a25e3fdf9e8b2c10852a3bec
GaxRodri <gaspar.unlp@gmail.com>
2024-04-05 20:07:22 +0200

    feat: create models folder and add subsequence model

---
+++ src/models/Subsequence.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, type Document } from 'mongoose';
+
+interface ISubsequence extends Document {
+  subsequences: [[number]];
+  createdAt: Date;
+}
+
+const SubsequenceSchema = new Schema<ISubsequence>({
+  subsequences: { type: [[Number]], required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Subsequence = mongoose.model<ISubsequence>(
+  'Subsequence',
+  SubsequenceSchema
+);
+
+export default Subsequence;
