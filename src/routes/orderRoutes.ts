e4d6fdee596949003d4c2409a82b939ce3756a95
GaxRodri <gaspar.unlp@gmail.com>
2024-04-06 21:36:11 +0200

    feat: create auth middleware and implement jwt as authentication method

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import * as orderController from '../controllers/orderController';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-router.post('/history', orderController.postOrderHistory);
-router.get('/recommendations', orderController.getRecommendations);
+router.post('/history', authMiddleware, orderController.postOrderHistory);
+router.get('/recommendations', authMiddleware, orderController.getRecommendations);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 
 import orderRoutes from './routes/orderRoutes';
+import authRoutes from './routes/authRoutes';
 import connectDB from './database/dbConnection';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
-app.use('/orders', orderRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api', authRoutes);
+
 
 void connectDB().then(() => {
-  console.log('Connected to MongoDB');
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
